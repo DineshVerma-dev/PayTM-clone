e2df177a36befd94d42590e90b907d484171cb86
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-12-23 13:53:27 +0530

    remoted the home components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,21 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Signup } from './pages/Signup';
 import { Signin } from './pages/Signin';
 import { Dashboard } from './pages/Dashboard';
 import { SendMoney } from './pages/SendMoney';
-import { Home } from './pages/Home';
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/send" element={<SendMoney />} />
-        
+
       </Routes>
     </Router>
   );
