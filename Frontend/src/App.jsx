043dad8f68ef79ee2ed1890546b5dcafa3c21284
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-12-23 13:21:16 +0530

    lets check

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,13 +5,13 @@ import { Signup } from './pages/Signup';
 import { Signin } from './pages/Signin';
 import { Dashboard } from './pages/Dashboard';
 import { SendMoney } from './pages/SendMoney';
-import { Home } from './pages/Home';
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+       
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
