694d30a03a2fb9971c409a00532db711e899ec91
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-01-02 00:18:46 +0530

    loading

--- Frontend/src/pages/SendMoney.jsx
+++ Frontend/src/pages/SendMoney.jsx
@@ -86,7 +86,7 @@ export const SendMoney = () => {
           <div className="p-6">
             <div className="flex items-center space-x-4">
               <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                <span className="text-2xl text-white">{name[0].toUpperCase()}</span>
+                <span className="text-2xl text-white">{name ? name[0].toUpperCase() : ''}</span>
               </div>
               <h3 className="text-2xl font-semibold">{name}</h3>
             </div>
